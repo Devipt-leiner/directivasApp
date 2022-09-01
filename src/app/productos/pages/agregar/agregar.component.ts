dde2b45dbd83d6afd67613c542889f60b568bee6
leiner.barrios@inglesparatodos.edu.co <devappipt@Devs-iMac.local>
2022-09-01 09:40:00 -0500

    Minor changes

--- src/app/productos/pages/agregar/agregar.component.ts
+++ src/app/productos/pages/agregar/agregar.component.ts
@@ -9,6 +9,9 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class AgregarComponent {
 
+  texto1: string = 'Leiner Barrios';
+  color : string = 'green';
+
   miFormulario: FormGroup = this.fb.group({
     nombre: ['', Validators.required]
   });
@@ -17,6 +20,15 @@ export class AgregarComponent {
 
   tieneError(control: string): boolean {
     return this.miFormulario.controls[control]?.invalid || false;
+  }
+
+  cambiarNombre() {
+    this.texto1 = Math.random().toString();
+  }
+
+  cambiarColor() {
+    const color = "#xxxxxx".replace(/x/g, y=>(Math.random()*16|0).toString(16));
+    this.color = color;
   }
 
 
--- src/app/shared/directives/error-msg.directive.ts
+++ src/app/shared/directives/error-msg.directive.ts
@@ -5,18 +5,40 @@ import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 })
 export class ErrorMsgDirective implements OnInit {
 
+  private _color: string = 'red';
+  private _mensaje: string = 'Este campo es requerido';
+
   htmlElement: ElementRef<HTMLElement>
-  @Input() color: string = 'red';
+
+  @Input() set color(valor: string) {
+    this._color = valor;
+    this.setColor();
+  }
+
+  @Input() set mensaje(valor: string) {
+    this._mensaje = valor;
+    this.setMensaje();
+  }
 
   constructor(private elementRef: ElementRef<HTMLElement>) {
     this.htmlElement = elementRef;
   }
 
   ngOnInit(): void {
-    this.setColor();
+    this.setEstilo();
+
+
+  }
+
+  setEstilo(): void {
+    this.htmlElement.nativeElement.classList.add('form-text');
   }
 
   setColor(): void {
-    this.htmlElement.nativeElement.style.color = this.color;
+    this.htmlElement.nativeElement.style.color = this._color;
+  }
+
+  setMensaje(): void {
+    this.htmlElement.nativeElement.innerText = this._mensaje;
   }
 }
